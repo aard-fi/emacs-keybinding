54390b30cc34d5015f9e2a6739cc4bbef1386a2f
Bernd Wachter <bernd-github@wachter.fi>
2026-04-22 16:43:09 +0300

    Drop jquery dependency

--- options.js
+++ options.js
@@ -1,42 +1,40 @@
-$(function() {
-  // this might trigger a bunch of superfluous sync.set directly after
-  // installation - but that's probably not worth bothering with.
-  $("input").on("change", function(){
-    var key=$(this).attr("id");
-    var type = $(this).attr("type");
+// this might trigger a bunch of superfluous sync.set directly after
+// installation - but that's probably not worth bothering with.
+document.querySelectorAll("input").forEach(function(input) {
+  input.addEventListener("change", function() {
+    var key = input.id;
+    var type = input.type;
     var value;
 
-    if (type == "checkbox")
-      value = $(this).get(0).checked;
-    else
-      value = $(this).get(0).value;
+    if (type === "checkbox") {
+      value = input.checked;
+    } else {
+      value = input.value;
+    }
 
-    //chrome.runtime.sendMessage({action: "log", msg: `Element: changed ${key}:${value}`});
-    chrome.runtime.sendMessage(
-      {action: "option", key: key, value: value});
+    chrome.runtime.sendMessage({action: "option", key: key, value: value});
     chrome.storage.sync.set({[key]: value});
   });
 });
 
 function restoreOptions() {
-  $("input").each(function () {
-    var key = $(this).attr("id");
-    var type = $(this).attr("type");
-    // to add keys without default value of false check a lookup table here
+  document.querySelectorAll("input").forEach(function(input) {
+    var key = input.id;
+    var type = input.type;
     var value = false;
-    chrome.storage.sync.get(key, function (setting) {
+
+    chrome.storage.sync.get(key, function(setting) {
       if (Object.keys(setting).length) {
         value = setting[key];
-        if (type == "checkbox")
-          $("#"+key).get(0).checked = value ? "checked" : undefined;
-        else
-          $("#"+key).get(0).value = value;
+        if (type === "checkbox") {
+          input.checked = value;
+        } else {
+          input.value = value;
+        }
       } else {
         chrome.storage.sync.set({[key]: value});
       }
-      //chrome.runtime.sendMessage({action: "log", msg: `Option: ${key}:${value} (${type})`});
-      chrome.runtime.sendMessage(
-        {action: "option", key: key, value: value});
+      chrome.runtime.sendMessage({action: "option", key: key, value: value});
     });
   });
 }
